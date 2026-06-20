db894832862fad9f13bc1ecd2e2f893432e4f9a0
vaishnaviinit <vaishnavipixy@gmail.com>
2026-06-20 13:25:33 +0530

    Add get student by id API

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -24,6 +24,7 @@ const createStudent = async (req, res) => {
   }
 };
 
+//read all students
 const getStudents = async (req, res) => {
   try {
 
@@ -41,8 +42,34 @@ const getStudents = async (req, res) => {
 
   }
 };
+
+
+//get student by id
+const getStudentById = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const result = await pool.query(
+      "SELECT * FROM students WHERE id = $1",    //safe from SQL injection used "$1" instead of "${id}"
+      [id]
+    );
+
+    if (result.rows.length === 0) {
+      return res.status(404).json({
+        message: "Student not found"
+      });
+    }
+    res.status(200).json(result.rows[0]);
+  }
+  catch (error) {
+    res.status(500).json({
+      message: error.message
+    });
+  }
+}
+
 module.exports = {
   createStudent,
-  getStudents
+  getStudents,
+  getStudentById
 };
-
--- routes/studentRoutes.js
+++ routes/studentRoutes.js
@@ -2,13 +2,15 @@ const express = require("express");
 
 const {
   createStudent,
-  getStudents
+  getStudents,
+  getStudentById
 } = require("../controllers/studentController");
 
 const router = express.Router();
 
 router.post("/", createStudent);
 router.get("/", getStudents);
+router.get("/:id",getStudentById);
 
 module.exports = router;
 
